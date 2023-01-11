e04324a95ad0c50ce9e56f2372a851a7ff3046fd
nekagit <nenad99@hotmail.de>
2023-01-11 15:05:54 +0100

    Auskommentiert

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
 import React, { useCallback } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 import { useState, useEffect } from 'react';
 
+// React Component showing Q@A Answers from qmBase most common questions
+// enabling a list of all ansers and a search&filter function
 function App() {
+
+  const maxleng: number = 200 // max length of Anwers
+  const [qa, setQa] = useState([]) // State fo fetched answers
   const [state, setState] = useState({
     query: '',
     list: []
-  })
-  const maxleng: number = 200
-  const [qa, setQa] = useState([])
-//   {
-//     "id": 1,
-//     "question": "Question 1",
-//     "answer": "Answer 1 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-//     "tags": [
-//         "Tag1",
-//         "Tag2"
-//     ]
-// },
+  }) // State of Searching
 
-  const handleChange = useCallback( (e: { target: { value: string; }; }) => {
+  // Fetching all answers
+  useEffect(() => {
+    fetch("https://qmbasefunctions.azurewebsites.net/api/questions?code=Y5DGbEq3YHjpTKgrwq9czVdm7ZxR8zy26Z_yNh8q4DFKAzFudvB65w==")
+      .then(response => response.json())
+      .then(data => setQa(data));
+  }, []);
+
+  // Think of memoization as caching a value so that it does not need to be recalculated.
+  // This allows us to isolate resource intensive functions so 
+  // that they will not automatically run on every render.
+  const handleChange = useCallback((e: { target: { value: string; }; }) => {
     const results = qa.filter((element: any) => {
       if (e.target.value === "") return qa
       if (element.question.toLowerCase().includes(e.target.value.toLowerCase())) {
@@ -39,12 +42,10 @@ function App() {
       query: e.target.value,
       list: results
     })
-  },[qa, setState])
-  useEffect(() => {
-    fetch("https://qmbasefunctions.azurewebsites.net/api/questions?code=Y5DGbEq3YHjpTKgrwq9czVdm7ZxR8zy26Z_yNh8q4DFKAzFudvB65w==")
-      .then(response => response.json())
-      .then(data => setQa(data));
-  }, []);
+  }, [qa, setState])
+
+  
+// TSX of the answers
   const listItems = qa.map((qa: any) =>
     <><li key={qa.id.toString()}><span className="qaQuestions">{qa.question}</span><br /><textarea
       value={qa.answer}
@@ -53,6 +54,8 @@ function App() {
       maxLength={maxleng}
     /></li></>
   );
+
+
   return (
     <div className="App">
       <header className="App-header">
